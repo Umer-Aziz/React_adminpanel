6bab0ec31b8b3d63a0ff653b192f4b14501cbe3f
Umer-Aziz <umars25271997@gmail.com>
2021-08-25 17:29:15 +0500

    added navbar and sidebar

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./topbar.css"
+import { NotificationsNone, Language, Settings } from "@material-ui/icons";
+import { Avatar } from '@material-ui/core';
+const Topbar = () => {
+    return (
+        <div className="topbar">
+            <div className="topbarWrapper">
+                <div className="topLeft">
+                    <span className="logo">umeradmin</span>
+                </div>
+                <div className="topRight">
+                    <div className="topbarIconContainer">
+                    <NotificationsNone/>
+                     <span className="topIconBag">2</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                    <Language/>
+                     <span className="topIconBag">1</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                    <Settings/>
+                    </div>
+                    <Avatar id="avatar" src="https://media-exp1.licdn.com/dms/image/C4D03AQFiU2yvc-NDWA/profile-displayphoto-shrink_100_100/0/1616837029697?e=1635379200&v=beta&t=Qppy0S2cSaq6yfYriDADEwBL94RfFZWlPfJZmwHOcqM"/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Topbar
