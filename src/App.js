ec11f2f92654ccf4ba7926f83dcfa9eaedb60c2e
Umer-Aziz <umars25271997@gmail.com>
2021-08-27 17:29:25 +0500

    added user

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import "./App.css";
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
 function App() {
   return (
-    <div>
+    <Router>
       <Topbar/>
       <div className="container">
       <Sidebar/>
+      <Switch>
+      <Route exact path="/">
       <Home/>
+      </Route>
+      <Route  path="/users">
+      <UserList/>
+      </Route>
+      <Route path="/user/:userId">
+            <User/>
+          </Route>
+      </Switch>
       </div>
-    </div>
+    </Router>
   );
 }
 
